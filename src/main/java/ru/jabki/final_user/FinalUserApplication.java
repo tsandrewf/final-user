dbd77606808e4706127ebac5fe73ef6dda2bea24
tsandrewf <tsandrewf@gmail.com>
2026-02-15 18:43:50 +0300

    init

---
+++ src/main/java/ru/jabki/final_user/FinalUserApplication.java
@@ -0,0 +1,13 @@
+package ru.jabki.final_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalUserApplication.class, args);
+	}
+
+}
