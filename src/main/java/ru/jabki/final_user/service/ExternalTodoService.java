27933fdb7508a6ff181aed25b79216decdee4bf1
tsandrewf <tsandrewf@gmail.com>
2026-03-04 18:08:29 +0300

    Исправления по замечаниям Ростислава #2

--- src/main/java/ru/jabki/final_user/service/ExternalTodoService.java
+++ src/main/java/ru/jabki/final_user/service/ExternalTodoService.java
@@ -1,37 +1,15 @@
 package ru.jabki.final_user.service;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-@AllArgsConstructor
 @Service
 public class ExternalTodoService {
 
     private final RestClient restClient;
 
-    public ExternalTodoService() {
-        String baseUrl = null;
-
-        {
-            Properties properties = new Properties();
-            InputStream inputStream =
-                    getClass().getClassLoader().getResourceAsStream("application.properties");
-            try {
-                properties.load(inputStream);
-                baseUrl = properties.getProperty("external.todo.service.baseurl");
-            } catch (IOException e) {
-
-            }
-        }
-        if (baseUrl == null) {
-            baseUrl = "http://localhost:8080/api/v1/todo";
-        }
-
+    public ExternalTodoService(@Value("${external.todo.service.baseurl:http://localhost:8080}") String baseUrl) {
         this.restClient = RestClient.builder()
                 .baseUrl(baseUrl)
                 .build();
@@ -41,7 +19,7 @@ public class ExternalTodoService {
 
         return Boolean.parseBoolean(restClient
                 .get()
-                .uri("/userByIdInvolved/{id}", userId)
+                .uri("/api/v1/todo/userByIdInvolved/{id}", userId)
                 .retrieve().body(String.class));
     }
 }
